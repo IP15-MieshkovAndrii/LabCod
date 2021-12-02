5136efbd287ffd5171bfb28a65c1b08a49ae64be
ІП-15 Мєшков Андрій <90135678+IP15-MieshkovAndrii@users.noreply.github.com>
2021-12-02 10:10:44 +0200

    Update lab7.cpp

--- lab7.cpp
+++ lab7.cpp
@@ -3,64 +3,65 @@
 #include <cstdio>
 #include <cstdlib>
 using namespace std;
-void create(char a[], char b[]);
-void output(char a[], char b[]);
-void third_arr(char a[], char b[], char c[]);
-void out_th(char c[]);
-void average(char c[]);
+void create(char a[], char b[], int n);
+void output(char a[], char b[], int n);
+void third_arr(char a[], char b[], char c[], int n);
+void out_th(char c[], int n);
+void average(char c[], int n);
 
 int main() {
-    char first[10], second[10], third[10];
-    create(first, second);
-    output(first, second);
-    third_arr(first, second, third);
-    out_th(third);
-    average(third);
+    const int n=10;
+    char first[n], second[n], third[n];
+    create(first, second, n);
+    output(first, second, n);
+    third_arr(first, second, third, n);
+    out_th(third, n);
+    average(third, n);
 }
 
-void create(char a[], char b[]){
-    for(int i=0; i<10; i++){
+void create(char a[], char b[], int n){
+    for(int i = 0; i < n; i++){
         a[i]=92 - 3 * i;
         b[i]=71 + 3 * i;
     }
 }
 
-void output(char a[], char b[]){
+void output(char a[], char b[], int n){
     cout << "First array: "<<endl;
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < n; i++){
         cout << a[i] << " ";
     }
     cout<< endl<<endl;
     cout << "Second array: "<<endl;
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < n; i++){
         cout << b[i] << " ";
     }
     cout<< endl<<endl;
 }
 
-void third_arr(char a[], char b[], char c[]){
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
+void third_arr(char a[], char b[], char c[], int n){
+    for(int i = 0; i < n; i++){
+        c[i]=0;
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
             if(a[i]==b[j]) c[i]=a[i];
         }
     }
-    for(int i = 0; i < 10; i++){
-        if (c[i] <= 32) c[i] = 0;
-    }
 }
 
-void out_th(char c[]){
+void out_th(char c[], int n){
     cout << "Third array: "<<endl;
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < n; i++){
         cout << c[i] << " ";
     }
     cout<< endl<<endl;
 }
 
-void average(char c[]){
+void average(char c[], int n){
     float av;
     int sum = 0, num=0;
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < n; i++){
         if(c[i]<82){sum+=c[i];
             if(c[i]!=0){
                 num++;
